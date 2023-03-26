d71f9f8b083f4140a1a663247e90a7e07e59b687
Max Zhang <0xc00005@gmail.com>
2023-03-26 13:28:31 -0400

    allow customize end trade time, and add candlestick and trade entry indication

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import talib
 import datetime
+import mplfinance as mpf
 
 # Get user input for start time
 start_time = input("Enter start time (yyyy-mm-dd): ")
 
+# Get user input for end time
+end_time = input("Enter end time (yyyy-mm-dd): ")
+# if end_time == "", set end_time to today
+if end_time == "":
+    end_time = datetime.date.today().strftime("%Y-%m-%d")
+
 # Download Apple stock data and S&P500 data
 apple = yf.Ticker("AAPL")
-apple_data = apple.history(start=start_time)
+apple_data = apple.history(start=start_time, end=end_time)
 
 sp500 = yf.Ticker("^GSPC")
-sp500_data = sp500.history(start=start_time)
+sp500_data = sp500.history(start=start_time, end=end_time)
 
 # Calculate MA10, RSI, and MACD for Apple stock
 apple_data["MA10"] = apple_data["Close"].rolling(window=10).mean()
@@ -38,7 +45,9 @@ sp500_total_profit = 0
 apple_data['DailyProfit'] = 0
 apple_data['Volume_shifted'] = apple_data['Volume'].shift(1)
 sp500_data['DailyProfit'] = 0
+apple_data['Trade'] = ""
 
+# The rest of the script remains the same until the end of the '__main__' section
 
 def execute_trade_A(row, in_trade, entry_price, exit_price, total_profit, MA10, RSI, MACD):
     daily_profit = 0
@@ -164,7 +173,22 @@ def test_multiple_strategies(execute_trade_functions, labels=None):
     print(f"S&P 500 Profit: {sp500_final_profit:.2%}")
 
 
-if __name__ == '__main__':
-    test_multiple_strategies([execute_trade_A, execute_trade_B, execute_trade_C], labels=["Strategy A", "Strategy B", "Strategy C"])
-    pass
 
+
+if __name__ == '__main__':
+    test_multiple_strategies([execute_trade_A, execute_trade_B, execute_trade_C],
+                             labels=["Strategy A", "Strategy B", "Strategy C"])
+
+    # Plotting the candlestick chart
+    plot_data = apple_data.loc[:, ['Open', 'High', 'Low', 'Close', 'Volume']]
+    fig, ax = mpf.plot(plot_data, type='candle', style='charles', volume=True, returnfig=True)
+    ax[0].set_title("Apple Stock - Candlestick Chart")
+
+    # Indicating opening and closing of each trade day
+    for i, row in apple_data.iterrows():
+        if row['Trade'] == "Entry":
+            ax[0].plot(i, row['Close'], marker='o', color='g', markersize=5)
+        elif row['Trade'] == "Exit":
+            ax[0].plot(i, row['Close'], marker='o', color='r', markersize=5)
+
+    plt.show()
