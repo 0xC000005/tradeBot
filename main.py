c8c645e29566f17d67b74d7abcfbccc60393ba50
Max Zhang <0xC00005@gmail.com>
2023-05-02 12:51:07 -0400

    limited the display range

--- main.py
+++ main.py
@@ -6,15 +6,23 @@ import pandas as pd
 
 # Get user input for start time
 start_time = input("Enter start time (yyyy-mm-dd): ")
+# turn it into datetime object
+start_time_datetime = datetime.datetime.strptime(start_time, "%Y-%m-%d")
 
 # Get user input for end time
 end_time = input("Enter end time (yyyy-mm-dd): ")
+# turn it into datetime object
+end_time_datetime = datetime.datetime.strptime(end_time, "%Y-%m-%d")
+
 # if end_time == "", set end_time to today
 if end_time == "":
     end_time = datetime.date.today().strftime("%Y-%m-%d")
 
+# Calculate the adjusted start date for downloading data (subtract 26 trading days)
+adjusted_start_date = datetime.datetime.strptime(start_time, "%Y-%m-%d") - datetime.timedelta(days=26 * 2)
+
 # Download the data using yfinance
-data_df = yf.download('AAPL', start=start_time, end=end_time)
+data_df = yf.download('AAPL', start=adjusted_start_date.strftime("%Y-%m-%d"), end=end_time)
 
 # Convert the data to backtrader compatible format
 data_df.index = pd.to_datetime(data_df.index)
@@ -42,9 +50,6 @@ class PandasData(btfeeds.PandasData):
 data = PandasData(dataname=data_df)
 
 
-# The rest of the code remains the same
-
-
 # Create a custom strategy
 class MyStrategy(bt.Strategy):
     params = (
@@ -53,14 +58,18 @@ class MyStrategy(bt.Strategy):
         ('macd_signal', 9),
         ('stop_loss', 0.02),
         ('take_profit', 0.05),
-        ('printlog', False),
+        ('printlog', True),
     )
 
     def __init__(self):
-        self.macd, self.macdsignal, self.macdhist = bt.indicators.MACDHisto(fastema=self.params.macd_fast,
-                                                                            slowema=self.params.macd_slow,
-                                                                            signal=self.params.macd_signal)
-
+        self.order = None
+        self.dataclose = self.datas[0].close
+        self.ma = bt.indicators.SimpleMovingAverage(self.datas[0], period=15)
+        self.rsi = bt.indicators.RSI_SMA(self.datas[0].close, period=21)
+        self.macd = bt.indicators.MACD(self.data.close, period_me1=self.params.macd_fast,
+                                       period_me2=self.params.macd_slow, period_signal=self.params.macd_signal)
+        self.macdsignal = self.macd.signal
+        self.macdhist = self.macd.macd - self.macd.signal
 
     def next(self):
         if self.order:
@@ -113,7 +122,7 @@ cerebro.run()
 print(f"Final Portfolio Value: {cerebro.broker.getvalue():.2f}")
 
 # Plot the result
-cerebro.plot(style='candlestick')
+cerebro.plot(style='candlestick', start=start_time_datetime, end=end_time_datetime)
 
 if __name__ == '__main__':
     pass
